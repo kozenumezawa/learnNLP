f62e65c24b489839f5371b9a546990cf98cd6e94
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2016-12-08 17:01:51 +0900

    Add: to study

---
+++ learn3.py
@@ -0,0 +1,17 @@
+# 引数として与えられたリストの中身を全て出力する関数を作ってください
+# 関数名: showElements
+
+
+fruits = ["apple", "banana", "orange"]
+
+showElements(fruits)
+# output: "apple", "banana", "orange"
+
+document1 = "I have a pen"
+document2 = "I have an apple"
+document3 = "Applepen!"
+document4 = "I have a pen"
+document5 = "I have apple and apple"
+
+documents = [document1, document2, document3, document4, document5]
+showElements(documents)
