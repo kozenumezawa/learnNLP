2b86cabfe0e043a08c629f1e40b8eb1d8d55c887
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2016-12-08 16:38:31 +0900

    Add:

---
+++ learn2.py
@@ -0,0 +1,9 @@
+# function to add two variables
+def plusFunction(x, y):
+    z = x + y
+    return z
+a = 1
+b = 2
+print(plusFunction(a, b))
+
+# function to subtraction
---
+++ test.py
@@ -0,0 +1,9 @@
+def tasizan(x, y):
+    z = x + y
+    return z
+
+a = 2
+b = 3
+
+c = tasizan(a, b)
+print(c)
