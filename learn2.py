7167ea8ca1bc60a6e9c6db8060fd3f39bad7c27b
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2016-12-10 10:22:44 +0900

    Add: to check output

--- learn2.py
+++ learn2.py
@@ -1,9 +1,13 @@
 # function to add two variables
 def plusFunction(x, y):
+    a = 3
     z = x + y
+    print(a)
     return z
 a = 1
-b = 2
+b = 3
+print(a)
 print(plusFunction(a, b))
+print(a)
 
 # function to subtraction
