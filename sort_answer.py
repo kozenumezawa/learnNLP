b057b2edfc9b5373f0a7e0e2d272192c604cba9c
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2016-12-22 17:35:34 +0900

    Update:

--- sort_answer.py
+++ sort_answer.py
@@ -1,23 +1,22 @@
 # coding: utf-8
 def find_min_index(number_list):
     min_index = 0
-    min_value = question[min_index]
+    min_value = number_list[min_index]
 
-    for i in range(1, len(question)):
-        if(question[i] < min_value):
+    for i in range(1, len(number_list)):
+        if(number_list[i] < min_value):
             min_index = i
-            min_value = question[min_index]
+            min_value = number_list[min_index]
     return min_index
 
 question = [5, 3, 2, 8, 9, 0, 4, 1, 6, 7]
 
-answer = []
-
 # sorting
-number_of_data = len(question)
-for i in range(number_of_data):
-    min_index = find_min_index(question)
-    answer.append(question[min_index])
-    del(question[min_index])
+for i in range(len(question)):
+    min_index = find_min_index(question[i:])
 
-print(answer)
+    min_value = question[min_index + i]
+    question[min_index + i] = question[i]
+    question[i] = min_value
+
+print(question)
--- sort_question.py
+++ sort_question.py
@@ -1,12 +1,12 @@
 # coding: utf-8
 def find_min_index(number_list):
     min_index = 0
-    min_value = question[min_index]
+    min_value = number_list[min_index]
 
-    for i in range(1, len(question)):
-        if(question[i] < min_value):
+    for i in range(1, len(number_list)):
+        if(number_list[i] < min_value):
             min_index = i
-            min_value = question[min_index]
+            min_value = number_list[min_index]
     return min_index
 
 question = [5, 3, 2, 8, 9, 0, 4, 1, 6, 7]
